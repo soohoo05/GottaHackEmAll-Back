754c4cd84bb1318fc2e7c316cb9a32197fb0463c
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2019-01-05 16:08:23 -0500

    removed To email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def hackathon_email(params)
     @info=params
-    mail(to: 'gottahackemall2@gmail.com ', subject: 'New Hackathon Submission')
+    mail(to: 'INSERT EMAIL HERE ', subject: 'New Hackathon Submission')
   end
 end
